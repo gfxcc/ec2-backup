ea1b82f7d6a3f68219641d9883f3de166328c9af
Yong Cao <ycao18@stevens.edu>
2016-04-10 15:00:59 -0400

    modify second part

--- ec2-backup.sh
+++ ec2-backup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # YongCao(ycao18)     Jignesh     Richard
 #
@@ -17,7 +17,7 @@ VOLUME_SIZE=''
 DIR_SIZE=''
 EC2_HOST=''
 VISINDICATED=''
-BACKUP_FLAG='--count 1 --instance-type t2.micro'
+BACKUP_FLAG='--instance-type t2.micro'
 
 #######################################
 #
@@ -69,10 +69,15 @@ if [ -z $DIRECTORY ]; then
 fi
 
 if [ ! -d "$DIRECTORY" ]; then
-    echo "${0}: ${DIRECTORY} No such directory" 	
+    echo "${0}: ${DIRECTORY} No such directory"
     exit 1
 fi
 
+
+create_volume () {
+
+    exit 0
+}
 ####################################
 #
 #if -v is indicated, check the size of volume
@@ -81,100 +86,155 @@ fi
 #Created by Richard
 #
 
-function CheckVolumeSize {
+check_volume () {
 
-VOLUME_SIZE=$(aws ec2 describe-volumes --volume-ids $VOLUME --query \
-    'Volumes[*].[Size]' --output text)
+    VOLUME_SIZE=$(aws ec2 describe-volumes --volume-ids $VOLUME --query \
+        'Volumes[*].[Size]' --output text)
 
-if [ $VOLUME_SIZE -ge `expr $DIR_SIZE \\* 2` ];then
+    if [ $VOLUME_SIZE -ge `expr $DIR_SIZE \\* 2` ];then
 
-    AVAILABILITY_ZONE=$(aws ec2 describe-volumes --volume-ids \
-        $VOLUME --query 'Volumes[*].[AvailabilityZone]' \
-        --output text)
+        AVAILABILITY_ZONE=$(aws ec2 describe-volumes --volume-ids \
+            $VOLUME --query 'Volumes[*].[AvailabilityZone]' \
+            --output text)
 
-else
-    exit 1
-fi
+    else
+        exit 1
+    fi
 }
 
-function CreateInstance{
+if [[ $VOLUME != "" ]]; then
+    check_volume
+else
+    create_volume
+fi
 
-INSTANCEID=('ami-fce3c696' 'ami-06116566'\
-    'ami-9abea4fb' 'ami-f95ef58a'\
-    'ami-87564feb' 'ami-a21529cc'\
-    'ami-09dc1267' 'ami-25c00c46'\
-    'ami-6c14310f' 'ami-0fb83963')
+#
+# create instance
+#
+IMAGE_ID[0]='ami-fce3c696'
+IMAGE_ID[1]='ami-06116566'
+IMAGE_ID[2]='ami-9abea4fb'
+IMAGE_ID[3]='ami-f95ef58a'
+IMAGE_ID[4]='ami-87564feb'
+IMAGE_ID[5]='ami-a21529cc'
+IMAGE_ID[6]='ami-09dc1267'
+IMAGE_ID[7]='ami-25c00c46'
+IMAGE_ID[8]='ami-6c14310f'
+IMAGE_ID[9]='ami-0fb83963'
 
-aws ec2 create-key-pair --key-name CS615KEY
+if [[ $REGION = "us-east-1" ]]; then
+    IMAGE_ID=IMAGE_ID[0]
+elif [[ $REGION = "us-west-1" ]]; then
+    IMAGE_ID=IMAGE_ID[1]
+elif [[ $REGION = "us-west-2" ]]; then
+    IMAGE_ID=IMAGE_ID[2]
+elif [[ $REGION = "eu-west-1" ]]; then
+    IMAGE_ID=IMAGE_ID[3]
+elif [[ $REGION = "eu-central-1" ]]; then
+    IMAGE_ID=IMAGE_ID[4]
+elif [[ $REGION = "ap-northeast-1" ]]; then
+    IMAGE_ID=IMAGE_ID[5]
+elif [[ $REGION = "ap-northeast-2" ]]; then
+    IMAGE_ID=IMAGE_ID[6]
+elif [[ $REGION = "ap-southeast-1" ]]; then
+    IMAGE_ID=IMAGE_ID[7]
+elif [[ $REGION = "ap-southeast-2" ]]; then
+    IMAGE_ID=IMAGE_ID[8]
+else [[ $REGION = "sa-east-1" ]]; then
+    IMAGE_ID=IMAGE_ID[9]
+fi
 
-aws ec2 create-security-group --group-name MY-SG 
+if [[ $EC2_BACKUP_VERBOSE != "" ]]; then
+    aws ec2 create-key-pair --key-name ec2_backup_KP
+else
+    aws ec2 create-key-pair --key-name ec2_backup_KP &>/dev/null
+fi
 
-aws ec2 authorize-security-group-ingress --group-name MY-SG --port 22 \
-    --protocol tcp --cidr 0.0.0.0/0
+if [[ $EC2_BACKUP_VERBOSE != "" ]]; then
+    aws ec2 create-security-group --group-name ec2_backup_SG
+else
+    aws ec2 create_security-group --group-name ec2_backup_SG &>/dev/null
+fi
+
+if [[ $EC2_BACKUP_VERBOSE != "" ]]; then
+    aws ec2 authorize-security-group-ingress --group-name \
+        ec2_backup_SG --port 22 --protocol tcp --cidr 0.0.0.0/0
+else
+    aws ec2 authorize-security-group-ingress --group-name \
+        ec2_backup_SG --port 22 --protocol tcp --cidr 0.0.0.0/0 &>/dev/null
+fi
+
 
 if [[ $EC2_BACKUP_FLAGS_AWS != "" ]]; then
-    INSTANCE=$(aws ec2 run-instances --image-id $IMAGE_ID --count 1 \
-        $BACKUP_FLAG --key-name CS615KEY --security-groups MY-SG \
-        --availability-zone $AVAILABILITY_ZONE  --output text --query\
+    INSTANCE=$(aws ec2 run-instances --image-id $IMAGE_ID \
+        $BACKUP_FLAG --key-name ec2_backup_KP --security-groups \
+        ec2_backup_SG --availability-zone $AVAILABILITY_ZONE \
+        --output text --query 'Instances[*].InstanceId')
+else
+    INSTANCE=$(aws ec2 run-instances --image-id $IMAGE_ID \
+        $EC2_BACKUP_FLAGS_AWS --key-name ec2_backup_KP \
+        --security-groups ec2_backup_SG --availability-zone \
+        $AVAILABILITY_ZONE --output text --query \
         'Instances[*].InstanceId')
 fi
-else
-    INSTANCE=$(aws ec2 run-instances --image-id $IMAGE_ID $BACKUP_FLAG \
-        --key-name CS615KEY --security-groups MY-SG --availability-zone\
-        $AVAILABILITY_ZONE --output text --query 'Instances[*].InstanceId')
+
+if [[ $EC2_BACKUP_VERBOSE != "" ]]; then
+    echo "Instance "$INSTANCE" was created"
 fi
 
-EC2_HOST=$(aws ec2 describe-instances --instance-ids $INSTANCE --query \
-    'Reservations[*].Instances[*].NetworkInterfaces.Association.\
-    PublicIp' --output text)
-}
+EC2_HOST=$(aws ec2 describe-instances --instance-ids $INSTANCE \
+    --output text --query 'Reservations[*].Instances[*].\
+    NetworkInterfaces.Association.PublicIp')
+
 
 aws ec2 attach-volume --volume-id $VOLUME --instance-id $INSTANCE \
-    --device /dev/xvdf 
+    --device /dev/xvdf
 
 ssh $EC2_BACKUP_FLAGS_SSH ubuntu@$EC2_HOST sudo mkfs -t ext4 /dev/xvdf
 
 MOUNT_DIR='~/MOUNT'
 
 ssh $EC2_BACKUP_FLAGS_SSH ubuntu@$EC2_HOST sudo mount /dev/xvdf MOUNT_DIR
+
+
 ######################################
 #
 # created by YongCao (ycao18)
 # execute backup dd/rsync
 # trap CTRL-C signal
-function backup {
-
-if [[ $EC2_BACKUP_VERBOSE != "" ]]; then
-    echo "[INFO] Start backup process"
-fi
-
-if [[ $METHOD = "dd" ]]; then
-    DATE=`date +%F_%T`
-    if [[ $EC2_BACKUP_VERBOSE != "" ]]; then
-        tar cvf $DIRECTORY | ssh $EC2_BACKUP_FLAGS_SSH \
-            ubuntu@$EC2_HOST dd of=$MOUNT_DIR/$DATE obs=512k
-    else
-        tar cf $DIRECTORY &>/dev/null | ssh $EC2_BACKUP_FLAGS_SSH \
-            ubuntu@$EC2_HOST dd of=$MOUNT_DIR/$DATE obs=512k &>/dev/null
-    fi
-fi
-
-if [[ $METHOD = "rsync" ]]; then
+backup () {
 
     if [[ $EC2_BACKUP_VERBOSE != "" ]]; then
-        rsync -avRc -e "ssh $EC2_BACKUP_FLAGS_SSH" $DIRECTORY ubuntu\
-            @$EC2_HOST:$MOUNT_DIR
-    else
-        rsync -aRc -e "ssh $EC2_BACKUP_FLAGS_SSH" $DIRECTORY ubuntu\
-            @$EC2_HOST:$MOUNT_DIR &>/dev/null
+        echo "[INFO] Start backup process"
     fi
-fi
 
-###
+    if [[ $METHOD = "dd" ]]; then
+        DATE=`date +%F_%T`
+        if [[ $EC2_BACKUP_VERBOSE != "" ]]; then
+            tar cvf $DIRECTORY | ssh $EC2_BACKUP_FLAGS_SSH \
+                ubuntu@$EC2_HOST dd of=$MOUNT_DIR/$DATE obs=512k
+        else
+            tar cf $DIRECTORY &>/dev/null | ssh $EC2_BACKUP_FLAGS_SSH \
+                ubuntu@$EC2_HOST dd of=$MOUNT_DIR/$DATE obs=512k &>/dev/null
+        fi
+    fi
 
-ssh $EC2_BACKUP_FLAGS_SSH ubuntu@$EC2_HOST sudo umount $MOUNT_DIR
+    if [[ $METHOD = "rsync" ]]; then
 
-ec2-detach-volume $VOLUME -i $INSTANCE
+        if [[ $EC2_BACKUP_VERBOSE != "" ]]; then
+            rsync -avRc -e "ssh $EC2_BACKUP_FLAGS_SSH" $DIRECTORY ubuntu\
+                @$EC2_HOST:$MOUNT_DIR
+        else
+            rsync -aRc -e "ssh $EC2_BACKUP_FLAGS_SSH" $DIRECTORY ubuntu\
+                @$EC2_HOST:$MOUNT_DIR &>/dev/null
+        fi
+    fi
+
+    ###
+
+    ssh $EC2_BACKUP_FLAGS_SSH ubuntu@$EC2_HOST sudo umount $MOUNT_DIR
+
+    ec2-detach-volume $VOLUME -i $INSTANCE
 
 }
 
